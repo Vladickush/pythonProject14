c69c50c5911caad93feedefe957b4c927ff20c72
Vladickush <u.vn@mail.ru>
2024-12-06 23:03:00 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,17 +1,32 @@
-#База продуктов
-
 import sqlite3
+
+connection = sqlite3.connect('database2.db')
+cursor = connection.cursor()
 
 def initiate_db():
     connection = sqlite3.connect('database2.db')
     cursor = connection.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS Products
-                 (id INTEGER PRIMARY KEY, 
-                 title TEXT NOT NULL, 
-                 description TEXT NOT NULL, 
-                 price INTEGER NOT NULL)''')
 
-    # Очистить базу
+
+    # Создать базу продуктов
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products
+    (id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price INTEGER NOT NULL)''')
+
+    # Создать базу клиентов
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users
+    (id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL)''')
+
+
+    # Очистить базу продуктов
     cursor.execute('DELETE FROM Products')
 
     # Заполнить базу продуктами
@@ -22,16 +37,31 @@ def initiate_db():
     connection.commit()
     connection.close()
 
-# Выбрать продукты из базы
-def get_all_products():
+
+# Заполняем базу клиентов
+def add_user(username, email, age):
     connection = sqlite3.connect('database2.db')
     cursor = connection.cursor()
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES(?, ?, ?, ?)",
+                  (username, email, age, 1000))
+    connection.commit()
+    connection.close()
+
+def is_included(username):
+    connection = sqlite3.connect('database2.db')
+    cursor = connection.cursor()
+
+    user = cursor.execute("SELECT * FROM Users WHERE username = ?", (username,))
+    if user.fetchone() is None:
+        return True
+    else:
+        return False
+
+
+# Выбрать продукты из базы
+def get_all_products():
     cursor.execute('SELECT * FROM Products')
     products = cursor.fetchall()
     connection.commit()
     connection.close()
     return products
-
-
-
-
